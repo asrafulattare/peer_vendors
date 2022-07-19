6c680ce7095dd20a239646c73383d8c4b3bf6643
26attare <26attare@gmail.com>
2022-07-19 16:04:26 +0600

    4:04

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:peer_vendors/screen/onboarding_screen.dart';
+
+import '../constants/color.dart';
+import '../constants/image_assets.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      backgroundColor: ColorBlock.white,
+      splashIconSize: MediaQuery.of(context).size.shortestSide * 0.7,
+      splash: Assets.logoSquar,
+      splashTransition: SplashTransition.fadeTransition,
+      nextScreen: const OnboardingScreen(),
+    );
+  }
+}
